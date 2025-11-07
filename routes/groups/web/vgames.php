f30c436115f974802308cf783588e4bd5cce4b19
gramelich <68996556+gramelich@users.noreply.github.com>
2025-11-07 10:46:03 -0300

    Update vgames.php

--- routes/groups/web/vgames.php
+++ routes/groups/web/vgames.php
@@ -7,7 +7,12 @@ Route::prefix('vgames')
     ->group(function ()
     {
         Route::get('/exclusive/', [VGamesController::class, 'index'])->name('index');
-        Route::get('/exclusive/{game}/{aposta}', [VGamesController::class, 'show'])->name('show');
-        Route::get('/exclusive/{game}', [VGamesController::class, 'show'])->name('show');
+
+        // Rota renomeada para 'show.aposta' para evitar conflito com a linha 9.
+        Route::get('/exclusive/{game}/{aposta}', [VGamesController::class, 'show'])->name('show.aposta'); 
+
+        // Esta é a rota que agora será chamada de 'web.vgames.show'
+        Route::get('/exclusive/{game}', [VGamesController::class, 'show'])->name('show'); 
+        
         Route::post('/game/sub', [VGamesController::class, 'subprocess'])->name('subprocess');
     });
