e97c226da94f28b3d966872616f0d301954eba9e
gramelich <68996556+gramelich@users.noreply.github.com>
2025-11-07 12:39:40 -0300

    Update VGamesController.php

--- app/Http/Controllers/Provider/Exclusives/VGamesController.php
+++ app/Http/Controllers/Provider/Exclusives/VGamesController.php
@@ -1,7 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Api\Providers;
-
+namespace App\Http\Controllers\Provider\Exclusives;
 use App\Helpers\Core as Helper;
 use App\Http\Controllers\Controller;
 use App\Models\GameBalance;
